074bef7db805f27c8e2b612e7835292db370adb8
JustChad1 <ramosrichard176@gmail.com>
2024-11-04 17:24:02 +0800

    initial commit

---
+++ userProfile.js
@@ -0,0 +1,10 @@
+let fullName = "Richard Michael V. Ramos";
+let age = "19";
+let favoriteNumber = "20";
+let favoriteColor = "Black";
+
+// Log it to the console//
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
